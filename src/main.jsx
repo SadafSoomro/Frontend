74aadb90ee5bdd54f5d953fabb52567d44494e08
Nida Shaikh <aleemnida57@gmail.com>
2026-06-12 16:19:17 +0500

    Update Google Client ID

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 import App from './App.jsx'
 
 // Hardcoding the Client ID that is in the backend .env as well for seamless frontend-backend auth
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "346283855204-69f0macsuurkdph1n6p6csujjh9u290a.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || "346283855204-tmhtfilf469h44b4jc8mri4oqokfk7jf.apps.googleusercontent.com";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
